006c8a015fe3631ad6e798cb2da44c424ca1ad34
Scillman <Scillman@users.noreply.github.com>
2024-02-09 21:15:07 +0100

    Compilable and runnable VeggycraftScreenHandler

--- src/main/java/net/klayil/veggycraft/mixin/CraftingTableBlockMixin.java
+++ src/main/java/net/klayil/veggycraft/mixin/CraftingTableBlockMixin.java
@@ -3,6 +3,7 @@ package net.klayil.veggycraft.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.klayil.veggycraft.screen.VeggycraftScreenHandler;
 import net.minecraft.block.BlockState;
@@ -18,10 +19,11 @@ import net.minecraft.world.World;
 public class CraftingTableBlockMixin
 {
     @Inject(method="createScreenHandlerFactory", at=@At("HEAD"), cancellable=true)
-    public NamedScreenHandlerFactory onCreateScreenHandlerFactory(BlockState state, World world, BlockPos blockPos)
+    public NamedScreenHandlerFactory onCreateScreenHandlerFactory(BlockState state, World world, BlockPos blockPos, CallbackInfoReturnable<NamedScreenHandlerFactory> cir)
     {
-        return new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> {
+        cir.setReturnValue(new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> {
             return new VeggycraftScreenHandler(syncId, inventory, ScreenHandlerContext.create(world, blockPos));
-        }, Text.translatable("container.crafting"));
+        }, Text.translatable("container.crafting")));
+        return cir.getReturnValue();
     }
 }
