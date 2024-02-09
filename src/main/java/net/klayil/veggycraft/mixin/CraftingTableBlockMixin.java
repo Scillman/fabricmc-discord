8e5a13bb8f084117c014f6aab28221da389192c1
Scillman <Scillman@users.noreply.github.com>
2024-02-09 21:07:11 +0100

    Added VeggycraftScreenHandler

---
+++ src/main/java/net/klayil/veggycraft/mixin/CraftingTableBlockMixin.java
@@ -0,0 +1,27 @@
+package net.klayil.veggycraft.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+import net.klayil.veggycraft.screen.VeggycraftScreenHandler;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(value=CraftingTableBlock.class)
+public class CraftingTableBlockMixin
+{
+    @Inject(method="createScreenHandlerFactory", at=@At("HEAD"), cancellable=true)
+    public NamedScreenHandlerFactory onCreateScreenHandlerFactory(BlockState state, World world, BlockPos blockPos)
+    {
+        return new SimpleNamedScreenHandlerFactory((syncId, inventory, player) -> {
+            return new VeggycraftScreenHandler(syncId, inventory, ScreenHandlerContext.create(world, blockPos));
+        }, Text.translatable("container.crafting"));
+    }
+}
---
+++ src/main/java/net/klayil/veggycraft/screen/VeggycraftScreenHandler.java
@@ -0,0 +1,93 @@
+package net.klayil.veggycraft.screen;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.RecipeInputInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeEntry;
+import net.minecraft.recipe.RecipeMatcher;
+import net.minecraft.recipe.book.RecipeBookCategory;
+import net.minecraft.screen.AbstractRecipeScreenHandler;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.ScreenHandlerType;
+
+public class VeggycraftScreenHandler extends AbstractRecipeScreenHandler<RecipeInputInventory>
+{
+    public VeggycraftScreenHandler(int syncId, PlayerInventory playerInventory)
+    {
+        this(syncId, playerInventory, ScreenHandlerContext.EMPTY);
+    }
+
+    public VeggycraftScreenHandler(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context)
+    {
+        super(ScreenHandlerType.CRAFTING, syncId);
+    }
+
+    @Override
+    public boolean canInsertIntoSlot(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canInsertIntoSlot'");
+    }
+
+    @Override
+    public void clearCraftingSlots() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'clearCraftingSlots'");
+    }
+
+    @Override
+    public RecipeBookCategory getCategory() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCategory'");
+    }
+
+    @Override
+    public int getCraftingHeight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCraftingHeight'");
+    }
+
+    @Override
+    public int getCraftingResultSlotIndex() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCraftingResultSlotIndex'");
+    }
+
+    @Override
+    public int getCraftingSlotCount() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCraftingSlotCount'");
+    }
+
+    @Override
+    public int getCraftingWidth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCraftingWidth'");
+    }
+
+    @Override
+    public boolean matches(RecipeEntry<? extends Recipe<RecipeInputInventory>> recipe) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'matches'");
+    }
+
+    @Override
+    public void populateRecipeFinder(RecipeMatcher finder) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'populateRecipeFinder'");
+    }
+
+    @Override
+    public boolean canUse(PlayerEntity player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'canUse'");
+    }
+
+    @Override
+    public ItemStack quickMove(PlayerEntity player, int slot) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quickMove'");
+    }
+    
+}
